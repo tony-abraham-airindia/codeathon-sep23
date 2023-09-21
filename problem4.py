1cd172c2a9e92386fc43013f84a6817850e06a99
Tony Abraham <tony.abraham@airindia.com>
2023-09-21 15:30:44 +0530

    commit problem 4 solution

---
+++ problem4.py
@@ -0,0 +1,5 @@
+#program to sort the given array of numbers in ascending order. Return the sorted array. Initial array size will be a maximum of 500 elements.
+
+def sort_list(list_number):
+    #start writing your code here
+    return sorted(list_number)
---
+++ test problem 4.py
@@ -0,0 +1,24 @@
+#test case for validating sort_list funtion
+from problem4 import sort_list
+
+def test_sort_list_1():
+    assert(sort_list([1,3,2])==[1,2,3])
+
+def test_sort_list_2():
+    assert(sort_list([1,3,2,4,5,6,3,7,8,9])==[1,2,3,3,4,5,6,7,8,9])
+
+def test_sort_list_3():
+    assert(sort_list([1,2,3,4,5,6,7,8,9])==[1,2,3,4,5,6,7,8,9])
+
+#test case with 50000 elements
+def test_sort_list_4():
+    list_number=[]
+    for i in range(50000):
+        list_number.append(i)
+    assert(sort_list(list_number)==list_number)
+
+    
+test_sort_list_1()
+test_sort_list_2()
+test_sort_list_3()
+test_sort_list_4()
